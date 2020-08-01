0d1d4c35d3f7ef8895d73b83e38c1edde0a8f6aa
hami <hamipour@gmail.com>
2020-08-01 13:02:16 +0430

    unit of work and repository first implementation

---
+++ MadPay724.Data/Infrastructure/Repository.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadPay724.Data.Infrastructure
+{
+    public class Repository<TEntity> : IRepository<TEntity>, IDisposable where TEntity : class
+    {
+        protected readonly DbContext _db;
+        protected readonly DbSet<TEntity> _dbSet;
+        public Repository(DbContext db)
+        {
+            _db = db;
+            _dbSet = _db.Set<TEntity>();
+        }
+
+        #region Delete
+        public void Delete(object id)
+        {
+            var entity = GetById(id);
+            if (entity == null)
+                throw new ArgumentException("couldnt find any value");
+
+            _dbSet.Remove(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void Delete(Expression<Func<TEntity, bool>> where)
+        {
+            IEnumerable<TEntity> entities = _dbSet.Where(where).AsEnumerable();
+            foreach(TEntity entity in entities)
+            {
+                _dbSet.Remove(entity);
+            }
+        }
+
+        
+
+      
+        #endregion
+
+        #region Get
+        public TEntity Get(Expression<Func<TEntity, bool>> where)
+        {
+            return _dbSet.Where(where).FirstOrDefault();
+        }
+
+        public TEntity GetById(object id)
+        {
+            return _dbSet.Find(id);
+        }
+        
+        public IEnumerable<TEntity> GetMany(Expression<Func<TEntity, bool>> where)
+        {
+            return _dbSet.Where(where).AsEnumerable();
+        }
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _dbSet.AsEnumerable();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> where)
+        {
+            return await _dbSet.Where(where).FirstOrDefaultAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(object id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetManyAsync(Expression<Func<TEntity, bool>> where)
+        {
+            return await _dbSet.Where(where).ToListAsync();
+        }
+        #endregion
+
+        #region insert
+        public void Insert(TEntity entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public async Task InsertAsync(TEntity entity)
+        {
+           await _dbSet.AddAsync(entity);
+        }
+        #endregion
+
+        #region update
+        public void Update(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentException("couldnt find any value");
+
+            _dbSet.Update(entity);
+        }
+
+
+        #endregion
+
+        #region dispose
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    _db.Dispose();
+                }
+            }
+            disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~Repository()
+        {
+            Dispose(false);
+        }
+        #endregion
+    }
+}
---
+++ MadPay724.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadPay724.Data.Infrastructure
+{
+    public class UnitOfWork<TContext> : IUnitOfWork<TContext> where TContext : DbContext, new()
+    {
+        protected readonly DbContext _db;
+        public UnitOfWork()
+        {
+            _db = new TContext();
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public Task<int> SaveAsync()
+        {
+            return  _db.SaveChangesAsync();
+        }
+
+        #region dispose method
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    _db.Dispose();
+                }
+            }
+            disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~UnitOfWork()
+        {
+            Dispose(false);
+        }
+        #endregion
+    }
+}
